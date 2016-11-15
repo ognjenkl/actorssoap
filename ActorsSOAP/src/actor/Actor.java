c420fdc1d5670b2c3d9ddea24e57ddf65a429102
ognjen <ognjenkl@gmail.com>
2016-11-15 15:09:55 +0100

    added insert actor funcitonality

--- ActorsSOAP/src/actor/Actor.java
+++ ActorsSOAP/src/actor/Actor.java
@@ -4,24 +4,21 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
-import org.apache.catalina.webresources.JarResource;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import dao.ConnectionPool;
 import dao.DaoFactory;
 import dao.DaoUtils;
-import dto.ActorDTO;
-
 /**
  * @author ognjen
  *
  */
 public class Actor {
 	private static final String SQL_GET_ACTORS = "SELECT * FROM zndfilm.actors WHERE name like ?;";
+	private static final String SQL_INSERT_ACTOR = "insert into actors (name) select * from (select ?) as tmp where not exists ( select name from actors where name = ?) limit 1;";
 
 	public String addActor(String actor){
 		System.out.print("SOAP add actor: " + actor);
@@ -41,7 +38,6 @@ public class Actor {
 			if(actorName == null) actorName = "";
 			pstmt = DaoUtils.prepareStatement(conn, SQL_GET_ACTORS, false, new Object[]{actorName + "%"});
 			rs = pstmt.executeQuery();
-			int count = 0;
 			while(rs.next()){
 				JSONObject jObject = new JSONObject();
 				jObject.put("id", rs.getInt("id")+"");
@@ -64,6 +60,31 @@ public class Actor {
 		
 		
 		return jArray.toString();
+	}
+	
+	public static boolean insertActor(String actorName){
+		Connection conn = null;
+		PreparedStatement ppst = null;
+		boolean retVal = false;
+		
+		try{
+			conn = ConnectionPool.getConnectionPool().checkOut();
+			ppst = conn.prepareStatement(SQL_INSERT_ACTOR);
+			ppst.setString(1, actorName);
+			ppst.setString(2, actorName);
+			
+			
+			if (ppst.executeUpdate() > 0)
+				retVal = true;
+		
+			ppst.close();
+			return retVal;
+		} catch (Exception e){
+			//to do log
+			return retVal;
+		} finally {
+			ConnectionPool.getConnectionPool().checkIn(conn);
+		}
 	}
 	
 }
